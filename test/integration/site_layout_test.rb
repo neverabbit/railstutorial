cea68ab8af92770c9d3c84ebee1c1af5c94fccba
neverabbit <neverrabbit@163.com>
2015-04-05 15:08:58 +0800

    4th chapter

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,7 +10,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
-    assert_select "a{href=?}", contact_path
+    assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
   
 end
