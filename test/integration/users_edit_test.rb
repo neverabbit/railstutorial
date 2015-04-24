71e971df5da60268464ff7d9df92cc3e424a9567
neverabbit <neverrabbit@163.com>
2015-04-24 16:40:27 +0800

    Excercise-Chapter9

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -35,11 +35,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to edit_user_path(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), user: { name: name, email: email, password: '', password_confirmation: ''}
+    patch user_path(@user), user: { name: name, email: email, password: '', password_confirmation: '' }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
     assert_equal @user.name, name
     assert_equal @user.email, email
+    assert_nil session[:forwarding_url]
   end
 end
